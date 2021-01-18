c7c3cfb603763f793ae7a90111b2dbc0215c9314
Eugene Hauptmann <eugene.hp2012@gmail.com>
2021-01-18 08:05:50 -0800

    added major  samples to the tests

--- __tests__/lines2tree.test.ts
+++ __tests__/lines2tree.test.ts
@@ -3,7 +3,24 @@ import { NEW_LINE } from '../src/constants'
 import { lines2tree } from '../src/lines2tree'
 
 describe("lines2tree - transform file to a json tree", () => {
-    const filenames = [`blank_description`,`google_birthday`]
+    const filenames = [
+        'blank_description',
+        'blank_line_end',
+        'blank_line_mid',
+        'daily_recur',
+        'day_long_recur_yearly',
+        'duration_instead_of_dtend',
+        'forced_types',
+        'google_birthday',
+        'minimal',
+        'multiple_rrules',
+        'only_dtstart_date',
+        'only_dtstart_time',
+        'parserv2',
+        'recur_instances',
+        'recur_instances_finite',
+        'utc_negative_zero',
+    ]
     filenames.map(filename => {
         it(`parses ${filename}.ics correctly`, () => {
             const file = fs.readFileSync(`./samples/ics/${filename}.ics`, 'utf-8')
--- src/wip.ts
+++ src/wip.ts
@@ -3,16 +3,36 @@ import { NEW_LINE } from './constants'
 import { lines2tree } from './lines2tree'
 
 // const file = fs.readFileSync('./samples/ics/blank_description.ics', 'utf-8')
-const file = fs.readFileSync('./samples/ics/google_birthday.ics', 'utf-8')
+// const file = fs.readFileSync('./samples/ics/google_birthday.ics', 'utf-8')
 
 const ics2json = (raw:string) => {
     const lines = raw.split(NEW_LINE)
     const obj = lines2tree(lines)
 
     // tslint:disable-next-line
-    console.log(JSON.stringify(obj, null, 2))
+    // console.log(JSON.stringify(obj, null, 2))
 
     return obj
 }
 
-ics2json(file)
+// ics2json(file)
+
+const PATH_IN = `./samples/ics`
+const PATH_OUT = `./samples/json`
+const files = fs.readdirSync(PATH_IN)
+
+files.map( filename => {
+    if(filename.includes('.ics')){
+        const f = filename.split('.')[0]
+        const filenameICS = `${PATH_IN}/${f}.ics`
+        const filenameJSON = `${PATH_OUT}/${f}.json`
+
+        const file = fs.readFileSync(filenameICS, 'utf-8')
+        const obj  = ics2json(file)
+        const json  = JSON.stringify(obj, null, 2)
+
+        fs.writeFileSync(filenameJSON, json)
+
+        console.log(`'${f}',`)
+    }
+})
